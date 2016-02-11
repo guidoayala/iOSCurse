67eda596edd5233cc1119404e5fe8ef896719941
Guido Ayala Andrade <guidoayala@hotmail.com>
2016-02-11 10:19:28 -0500

    First Commit

--- HolaiPad/ViewController.swift
+++ HolaiPad/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var labelHolaMundo: UILabel!
+    @IBOutlet weak var labelMSN: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        labelHolaMundo.text = "Hola \(textFieldName.text!)"
+        view.endEditing(true)
+        
+    }
 
+    
+    @IBAction func bottonName(sender: AnyObject) {
+        labelMSN.text = "Guido "
+    }
+    @IBAction func buttonLastName(sender: AnyObject){
+        labelMSN.text = "Ayala  "
+    }
+    
+    @IBAction func buttonCity(sender: AnyObject) {
+        labelMSN.text = "Cúcuta"
+    }
+    
+    @IBAction func buttonCountry(sender: AnyObject) {
+    }
+   
 }
 
